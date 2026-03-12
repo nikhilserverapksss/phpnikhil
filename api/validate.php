02289ac4ab33086ae196bd8d20641cb85ba36d67
nikhilserverapksss <soneyadasray7890@gmail.com>
2026-03-12 07:08:01 -0700

    Update validate.php

--- api/validate.php
+++ api/validate.php
@@ -63,7 +63,7 @@ function encryptConfig($jsonData) {
 
 // Valid keys
 $validKeys = [
-    'SLOVEO-261354332176-Keyee242211-2126',
+    'SLOVEO-261354332176-key000000007-2126',
     'S-2026',
     '-2026'
 ];
@@ -98,6 +98,7 @@ $encryptedConfig = encryptConfig(json_encode($config));
 http_response_code(200);
 echo json_encode(['status' => 'ok', 'data' => $encryptedConfig]);
 ?>
+
 
 
 
